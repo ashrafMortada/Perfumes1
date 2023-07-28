837305203dad0439df6e71fb7935e6fbfab0ffb6
ashrafMortada <ashrafgeology123@gmail.com>
2023-07-28 17:58:48 +0300

    Perfumes1

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var allproducts = document.querySelectorAll(".list li")
+var content = document.querySelector("#content")
+var btn = document.querySelector("#btn1")
+var totalPrice = 0
+
+allproducts.forEach (function (item){
+    item.onclick = function (){
+
+        totalPrice += +(item.getAttribute("price"))
+        content.innerHTML +=item.textContent + " * "
+   
+        if ( content.innerHTML != " "){
+
+         btn.style.display = "block" 
+        }
+    }
+})
+
+btn.onclick = function(){
+
+    document.getElementById("Total").innerHTML = (totalPrice)
+}
